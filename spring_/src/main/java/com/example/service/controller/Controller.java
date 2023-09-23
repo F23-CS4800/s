236e348306275fc4839ab8b3c1bd5f6bb28bce1a
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-09-23 15:20:48 -0700

    cc

--- spring_/src/main/java/com/example/service/controller/Controller.java
+++ spring_/src/main/java/com/example/service/controller/Controller.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Controller {
 
-
-
     @GetMapping("/message")
     public Greeting getMessage(@RequestParam(name = "message", defaultValue = "Hello, world!") String message) {
         return new Greeting(message);
