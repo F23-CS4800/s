ba224299d2403a3b03dda69aa6ce3ba8a9de8f44
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-09-23 14:55:23 -0700

    fixed structure

--- spring_/src/main/java/com/example/service/controller/Controller.java
+++ spring_/src/main/java/com/example/service/controller/Controller.java
@@ -1,14 +1,16 @@
 package com.example.service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.example.service.models.Greeting;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/greeting")
-    public String getMessage() {
-        return "Hello, world!";
+
+
+    @GetMapping("/message")
+    public Greeting getMessage(@RequestParam(name = "message", defaultValue = "Hello, world!") String message) {
+        return new Greeting(message);
     }
+
 }
