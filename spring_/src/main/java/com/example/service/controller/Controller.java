ed379577396d2597d29c9876f4895080a8280b3e
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-09-23 14:04:23 -0700

    fixing the project structure

--- spring_/src/main/java/com/example/service/controller/Controller.java
+++ spring_/src/main/java/com/example/service/controller/Controller.java
@@ -1,13 +1,14 @@
-package com.example;
+package com.example.service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.service.models.Greeting;
 
 @RestController
 public class Controller {
 
     @GetMapping("/greeting")
-    public String getGreeting() {
+    public String getMessage() {
         return "Hello, world!";
     }
 }
---
+++ spring_/src/main/java/com/example/service/models/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.service.models;
+
+public class Greeting {
+    public String message;
+
+    public Greeting(String message) { this.message = message;
+    }
+
+    public String getMessage() { return message;
+    }
+
+    public void setMessage(String message) { this.message = message;
+    }
+}
