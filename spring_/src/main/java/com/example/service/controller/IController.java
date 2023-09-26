4e99f6d3aa3b8065166e549c645b2b768aa85a99
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-09-26 09:32:44 -0700

    JSOUP example added

--- spring_/src/main/java/com/example/service/controller/IController.java
+++ spring_/src/main/java/com/example/service/controller/IController.java
@@ -3,13 +3,7 @@ package com.example.service.controller;
 import com.example.service.models.Greeting;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-
 
 @RestController
 public class IController
