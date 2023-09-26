64ab899680a47e5da54bf70811301b5ebc974856
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-09-26 08:54:00 -0700

    added dependecies

--- spring_/src/main/java/com/example/service/controller/IController.java
+++ spring_/src/main/java/com/example/service/controller/IController.java
@@ -1,10 +1,19 @@
 package com.example.service.controller;
 
 import com.example.service.models.Greeting;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
 @RestController
-public class Controller {
+public class IController
+{
 
     @GetMapping("/message")
     public Greeting getMessage(@RequestParam(name = "message", defaultValue = "Hello, world!") String message) {
