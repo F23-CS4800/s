1172283c50a57b8451630aadf93b5ea5c503c61d
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-10-05 09:40:23 -0700

    ..

--- spring_/src/main/java/com/lookout/service/repository/CrimeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lookout.service.repository;
-
-import com.lookout.service.models.Crime;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CrimeRepository extends MongoRepository<Crime, String> {
-    // define custom query methods here if needed
-}
-
---
+++ spring_/src/test/java/spring/spring_/ControllerTest/GreetingControllerTest.java
@@ -0,0 +1,42 @@
+package spring.spring_.ControllerTest;
+
+import com.lookout.service.controller.IController;
+import com.lookout.service.models.Greeting;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreetingControllerTest {
+
+    @Test
+    public void testGetMessageWithDefaultValue() {
+        // Create an instance of your controller
+        IController yourController = new IController();
+
+        // Set up a default value for the @RequestParam
+        String defaultValue = "Hello, world!";
+
+        // Call the getMessage method with the default value
+        Greeting greeting = yourController.getMessage(defaultValue);
+
+        // Check if the message in the Greeting object matches the default value
+        assertEquals(defaultValue, greeting.getMessage());
+    }
+
+    @Test
+    public void testGetMessageWithCustomValue() {
+        // Create an instance of your controller
+      IController yourController = new IController();
+
+        // Set up a custom value for the @RequestParam
+        String customValue = "Custom Message";
+
+        // Call the getMessage method with the custom value
+        Greeting greeting = yourController.getMessage(customValue);
+
+        // Check if the message in the Greeting object matches the custom value
+        assertEquals(customValue, greeting.getMessage());
+    }
+}
+
