ad54bf101f9f4b824b0c9f567238eb32841f8e51
Gelila21 <79760415+Gelila21@users.noreply.github.com>
2023-10-05 14:43:08 -0700

    Update GreetingControllerTest.java

--- spring_/src/test/java/spring/spring_/ControllerTest/GreetingControllerTest.java
+++ spring_/src/test/java/spring/spring_/ControllerTest/GreetingControllerTest.java
@@ -11,31 +11,19 @@ public class GreetingControllerTest {
 
     @Test
     public void testGetMessageWithDefaultValue() {
-        // Create an instance of controller
         IController yourController = new IController();
-
-        // Set up a default value for the @RequestParam
         String defaultValue = "Hello, world!";
 
-        // Call the getMessage method with the default value
         Greeting greeting = yourController.getMessage(defaultValue);
-
-        // Check if the message in the Greeting object matches the default value
         assertEquals(defaultValue, greeting.getMessage());
     }
 
     @Test
     public void testGetMessageWithCustomValue() {
-        // Create an instance of controller
       IController yourController = new IController();
-
-        // Set up a custom value for the @RequestParam
         String customValue = "Custom Message";
-
-        // Call the getMessage method with the custom value
+        
         Greeting greeting = yourController.getMessage(customValue);
-
-        // Check if the message in the Greeting object matches the custom value
         assertEquals(customValue, greeting.getMessage());
     }
 }
