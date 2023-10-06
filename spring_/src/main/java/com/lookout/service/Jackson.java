0e6d465a8a8d81c65bf452aa5ac89c54757571b4
uhnguyen101 <70361768+uhnguyen101@users.noreply.github.com>
2023-10-05 20:37:42 -0700

    added new method

--- spring_/src/main/java/com/lookout/service/Jackson.java
+++ spring_/src/main/java/com/lookout/service/Jackson.java
@@ -1,10 +1,12 @@
+package com.lookout.service.jackson;
+
 import java.io.IOException;
 import java.util.Iterator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
 
-public class JacksonTest {
+public class Jackson {
 	public static void main(String args[]){
 		ObjectMapper map = new ObjectMapper();
 		String json = "{\"crime\":\"petty theft\", \"date occurred\":10/03/22}";
@@ -15,4 +17,11 @@ public class JacksonTest {
 		JsonNode date = root.path("date occurred");
 		System.out.println("Date Occurred: " + date.textValue());
 	}
+	public Size getSize(String json) {
+		ObjectMapper map = new ObjectMapper();
+		JsonNode root = map.readTree(json);
+		List<JsonNode> list = root.findParents("crime");
+		return(list.size());
+	}
+
 }
